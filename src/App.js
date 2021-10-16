366dbfeeb182734eca0188cf6914b94c0b70cf01
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-10-16 14:44:59 -0500

    :art: [ADD] Context component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,25 @@
-import React, { useState } from 'react'
-import { TodoCounter, TodoSearch, TodoList, TodoItem, CreateTodoButton, Message } from './components'
-import { useLocalStorage } from './lib/useLocalStorage'
-
-// const defaultTodos = [
-//   { id: 1, text: 'Create New Todo', completed: false },
-//   { id: 2, text: 'Create New Todo 2', completed: false },
-//   { id: 3, text: 'Create New Todo 3', completed: false },
-//   { id: 4, text: 'Create New Todo 4', completed: false },
-// ]
-
-const TODO_VERSION = 'TODOS_V1'
+import React from 'react'
+import { TodoProvider, Context, TodoCounter, TodoSearch, TodoList, TodoItem, CreateTodoButton, Message } from './components'
 
 function App() {
-  const { item: todos, saveItem: saveTodos, loading, error } = useLocalStorage(TODO_VERSION, []) // * Custom Hook
-  const [searchValue, setSearchValue] = useState('')
-
-  const completedTodos = todos.filter((todo) => !!todo.completed).length // * Shortcut for conditional true
-  const totalTodos = todos.length
-
-  let searchedTodos = []
-  if (searchValue < 1) {
-    searchedTodos = todos
-  } else {
-    searchedTodos = todos.filter((todo) => todo.text.toLowerCase().includes(searchValue.toLowerCase()))
-  }
-
-  const handleCompleted = (id) => {
-    const todoIndex = todos.findIndex((todo) => todo.id === id)
-    const newTodos = [...todos]
-    newTodos[todoIndex].completed = !newTodos[todoIndex].completed
-    saveTodos(newTodos)
-  }
-
-  const handleDelete = (id) => {
-    const todoIndex = todos.findIndex((todo) => todo.id === id)
-    const newTodos = [...todos]
-    newTodos.splice(todoIndex, 1)
-    saveTodos(newTodos)
-  }
-
   return (
-    <>
-      <TodoCounter total={totalTodos} completed={completedTodos} />
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-      <TodoList>
-        {error && <Message text={'An error occurred'} />}
-        {loading && <Message text={'Loading...'} />}
-        {!loading && !searchedTodos.length && <Message text={'Create your first TODO !'} />}
-        {searchedTodos.map((todo) => (
-          <TodoItem key={todo.id} text={todo.text} completed={todo.completed} handleComplete={() => handleCompleted(todo.id)} handleDelete={() => handleDelete(todo.id)} />
-        ))}
-      </TodoList>
+    <TodoProvider>
+      <TodoCounter />
+      <TodoSearch />
+      <Context.Consumer>
+        {({ error, loading, searchedTodos, handleCompleted, handleDelete }) => (
+          <TodoList>
+            {error && <Message text={'An error occurred'} />}
+            {loading && <Message text={'Loading...'} />}
+            {!loading && !searchedTodos.length && <Message text={'Create your first TODO !'} />}
+            {searchedTodos.map((todo) => (
+              <TodoItem key={todo.id} text={todo.text} completed={todo.completed} handleComplete={() => handleCompleted(todo.id)} handleDelete={() => handleDelete(todo.id)} />
+            ))}
+          </TodoList>
+        )}
+      </Context.Consumer>
       <CreateTodoButton />
-    </>
+    </TodoProvider>
   )
 }
 
---
+++ src/components/todoContext/TodoContext.js
@@ -0,0 +1,54 @@
+import React, { createContext, useState } from 'react'
+import { useLocalStorage } from '../../lib/useLocalStorage'
+
+const TODO_VERSION = 'TODOS_V1'
+const Context = createContext() // * For create a context
+
+function TodoProvider({ children }) {
+  const { item: todos, saveItem: saveTodos, loading, error } = useLocalStorage(TODO_VERSION, []) // * Custom Hook
+  const [searchValue, setSearchValue] = useState('')
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length // * Shortcut for conditional true
+  const totalTodos = todos.length
+
+  let searchedTodos = []
+  if (searchValue < 1) {
+    searchedTodos = todos
+  } else {
+    searchedTodos = todos.filter((todo) => todo.text.toLowerCase().includes(searchValue.toLowerCase()))
+  }
+
+  const handleCompleted = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id)
+    const newTodos = [...todos]
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed
+    saveTodos(newTodos)
+  }
+
+  const handleDelete = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id)
+    const newTodos = [...todos]
+    newTodos.splice(todoIndex, 1)
+    saveTodos(newTodos)
+  }
+
+  // * The values are the states that we want to share in our application
+  return (
+    <Context.Provider
+      value={{
+        loading,
+        error,
+        totalTodos,
+        completedTodos,
+        searchValue,
+        setSearchValue,
+        searchedTodos,
+        handleCompleted,
+        handleDelete,
+      }}>
+      {children}
+    </Context.Provider>
+  )
+}
+
+export { TodoProvider, Context }
